b2ad0acdb95ee856bf69207948eca150c3a61aec
Salsabilla Rachma <salsabillarachma8@gmail.com>
2023-12-12 16:17:05 +0700

    Update assessing_order_payments.py

--- 02. Assessing Data/assessing_order_payments.py
+++ 02. Assessing Data/assessing_order_payments.py
@@ -1,1 +1,11 @@
+#The number and type of customer data will be checked.
+order_items_df.info()
+#It has been found that the quantity and type of data are correct.
 
+#Next, a check will be conducted for duplicate data.
+print('jumlah duplicate :',order_items_df.duplicated().sum())
+#It has been found that there is no duplicate data.
+
+#Next, a check will be conducted for null values.
+order_items_df.isna().sum()
+#It has been found that there are no null values.
