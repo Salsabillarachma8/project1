0073f6473c46cee3aeeb5be04fc80ef474c677bc
Salsabilla Rachma <salsabillarachma8@gmail.com>
2023-12-12 15:59:50 +0700

    gethering.py

--- 01. Gethering Data/gethering.py
+++ 01. Gethering Data/gethering.py
@@ -5,12 +5,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Gathering Data
-customers_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/customers_dataset.csv")
-order_items_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/order_items_dataset.csv")
-order_payments_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/order_payments_dataset.csv")
-order_reviews_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/order_reviews_dataset.csv")
-orders_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/orders_dataset.csv")
-product_category_name_translation_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/product_category_name_translation.csv")
-products_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/products_dataset.csv")
-sellers_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma/Dashboard1/main/sellers_dataset.csv")
-
+customers_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/customers_dataset.csv")
+order_items_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/order_items_dataset.csv")
+order_payments_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/order_payments_dataset.csv")
+order_reviews_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/order_reviews_dataset.csv")
+orders_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/orders_dataset.csv")
+product_category_name_translation_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/product_category_name_translation.csv")
+products_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/products_dataset.csv")
+sellers_df = pd.read_csv("https://raw.githubusercontent.com/Salsabillarachma8/project1/main/00.%20Data/sellers_dataset.csv")
