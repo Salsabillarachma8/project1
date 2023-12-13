1f4c5c01c3d28ce11fad2fd1ca72abd4f49eb414
Salsabilla Rachma <salsabillarachma8@gmail.com>
2023-12-13 14:51:36 +0700

    Update visualization2.py

--- 05. Visualization with python/visualization2.py
+++ 05. Visualization with python/visualization2.py
@@ -12,8 +12,8 @@ sns.barplot(
     data=byproduct_df.sort_values(by="product_name_count", ascending=False).head(5),
     palette=colors
 )
-plt.title("5 product yang sering diorder", loc="center", fontsize=15)
-plt.ylabel(None)
-plt.xlabel(None)
+plt.title("5 most purchased products", loc="center", fontsize=15)
+plt.ylabel("quantity", fontsize=10)
+plt.xlabel("product", fontsize=10)
 plt.tick_params(axis='x', labelsize=10)
 plt.show()
