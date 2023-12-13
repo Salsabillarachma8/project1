14cae2139efa7257b4b605fffde51b27624d7146
Salsabilla Rachma <salsabillarachma8@gmail.com>
2023-12-13 14:44:03 +0700

    Update visualization_rfm.py

--- 05. Visualization with python/visualization_rfm.py
+++ 05. Visualization with python/visualization_rfm.py
@@ -1,24 +1,47 @@
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 6))
+#By Recency (days)
+fig, ax = plt.subplots(figsize=(10, 6))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+ 
+sns.barplot(
+    y="recency",
+    x="customer_id",
+    data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors,
+)
+ax.set_title("By Recency (days)", loc="center", fontsize=18)
+ax.set_ylabel("recency", fontsize=15)
+ax.set_xlabel("customer_id", fontsize=15)
+ax.tick_params(axis='x', labelsize=5)
+ax.tick_params(axis='y', labelsize=10)
+plt.show()
 
-colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
+#frequency
+fig, ax = plt.subplots(figsize=(10, 6))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+ 
+sns.barplot(
+    y="frequency",
+    x="customer_id",
+    data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors
+)
+ax.set_title("By Frequency", loc="center", fontsize=18)
+ax.set_ylabel("frequency", fontsize=15)
+ax.set_xlabel("customer_id", fontsize=15)
+ax.tick_params(axis='x', labelsize=5)
+ax.tick_params(axis='y', labelsize=10)
+plt.show()
 
-sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
-ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
-ax[0].tick_params(axis ='x', labelsize=15)
-
-sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
-ax[1].set_title("By Frequency", loc="center", fontsize=18)
-ax[1].tick_params(axis='x', labelsize=15)
-
-sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
-ax[2].set_ylabel(None)
-ax[2].set_xlabel(None)
-ax[2].set_title("By Monetary", loc="center", fontsize=18)
-ax[2].tick_params(axis='x', labelsize=15)
-
-plt.suptitle("Best Customer Based on RFM Parameters (customer_id)", fontsize=20)
+#monetary
+fig, ax = plt.subplots(figsize=(10, 6))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+ 
+sns.barplot(
+    y="monetary",
+    x="customer_id",
+    data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors
+)
+ax.set_title("By Monetary", loc="center", fontsize=18)
+ax.set_ylabel("monetary", fontsize=15)
+ax.set_xlabel("customer_id", fontsize=15)
+ax.tick_params(axis='x', labelsize=5)
+ax.tick_params(axis='y', labelsize=10)
 plt.show()
